6c48caa1cb2a11d50c40fcc7cf59769cc1697500
Smit Vekaria <smit.vce19@sot.pdpu.ac.in>
2021-04-20 19:28:55 +0530

    Showcased price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -66,6 +66,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
+          price={this.state.totalPrice}
         />
       </>
     );
