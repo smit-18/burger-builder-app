8b0c95a623bd823854cce8ce1636e96153c2da41
Smit Vekaria <smit.vce19@sot.pdpu.ac.in>
2021-04-20 12:14:14 +0530

    Rendering ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 import classes from './Burger.module.css';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map((igKey) => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        });
     return (
       <div className={classes.Burger}>
-        <BurgerIngredients type="bread-top" />
-        <BurgerIngredients type="cheese" />
-        <BurgerIngredients type="meat" />
-        <BurgerIngredients type="bread-bottom" />
+        <BurgerIngredient type="bread-top" />
+        {transformedIngredients}
+        <BurgerIngredient type="bread-bottom" />
       </div>
     );
 }
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-import classes from "./BurgerIngredients.module.css";
+import classes from "./BurgerIngredient.module.css";
 
-class BurgerIngredients extends Component {
+class BurgerIngredient extends Component {
   render() {
     let ingredient = null;
 
@@ -46,8 +46,8 @@ class BurgerIngredients extends Component {
   }
 }
 
-BurgerIngredients.propTypes = {
+BurgerIngredient.propTypes = {
   type: PropTypes.string.isRequired,
 };
 
-export default BurgerIngredients;
+export default BurgerIngredient;
