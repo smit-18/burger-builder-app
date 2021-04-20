daad535a6bf8f31265e7b536e90ee82ee2b5de9a
Smit Vekaria <smit.vce19@sot.pdpu.ac.in>
2021-04-20 12:54:29 +0530

    Added build control component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './BuildControls.module.css';
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        
+    </div>
+);
+
+
+
+export default buildControls;
