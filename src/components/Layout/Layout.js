b2ae063723af0d3f235e59acdee4f43a4183d9fa
Smit Vekaria <smit.vce19@sot.pdpu.ac.in>
2021-04-21 22:55:02 +0530

    Adding navigation component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './Layout.module.css';
 
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 const layout = (props) => (
     <>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.content}>
+        <Toolbar />
+        <main className={classes.Content}>
             {props.children}
         </main>
     </>
