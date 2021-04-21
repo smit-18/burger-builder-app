84e7730f7948b1405f67cd7458efd1c2eff984d0
Smit Vekaria <smit.vce19@sot.pdpu.ac.in>
2021-04-21 22:31:00 +0530

    Added custom button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,6 +17,8 @@ const orderSummary = (props) => {
           {ingredientSummary}
       </ul>
       <p>Continue to checkout?</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </>
   );
 };
