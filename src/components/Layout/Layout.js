da7e5ae3328ad4c41823ea7b796b0c1c467cba30
Smit Vekaria <smit.vce19@sot.pdpu.ac.in>
2021-04-21 23:05:37 +0530

    Adding logo component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import classes from './Layout.module.css';
+import React from "react";
+import classes from "./Layout.module.css";
 
-import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = (props) => (
-    <>
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </>
+  <>
+    <Toolbar />
+    <main className={classes.Content}>{props.children}</main>
+  </>
 );
 
 export default layout;
